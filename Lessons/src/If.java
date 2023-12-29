b8b21ce32a47b16c44047f1184a7e0ed115e9c52
Aleh1 <krytiak3@gmail.com>
2023-12-29 23:23:46 +0300

    if

---
+++ Lessons/src/If.java
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+public class If {
+    public  static void main(String [] args) {
+        int myInt = 5;
+        if (myInt<10){
+            System.out.println("да, верно.");
+        } else {
+            System.out.println("Нет, не верно");
+        }
+    }
+}
